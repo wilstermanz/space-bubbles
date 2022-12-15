79ee4677afb1abc24c1371489955681626b2a18b
bsbanotto <ben.sbanotto@gmail.com>
2022-12-15 10:38:40 -0600

    Updated bullets to dart images

--- models/bullet.py
+++ models/bullet.py
@@ -13,8 +13,19 @@ BLACK = (30, 30, 30)
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, pos, speed, screen_height, color):
         super().__init__()
+        if color == WHITE:
+            color = 'white'
+        elif color == RED:
+            color = 'red'
+        elif color == GREEN:
+            color = 'green'
+        elif color == YELLOW:
+            color = 'yellow'
+        else:
+            color = 'blue' 
+        file_path = 'images/' + color + '_dart.png'
         self.image = pygame.Surface((16, 16))
-        self.image.fill(color)
+        self.image = pygame.image.load(file_path).convert_alpha()
         self.rect = self.image.get_rect(center = pos)
         self.speed = speed
         self.height_y_constraint = screen_height

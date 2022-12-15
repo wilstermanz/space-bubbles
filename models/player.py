8a2065d9bb862d9b699ce63b1aa6ec3025b08e97
wilstermanz <wilstermanz@gmail.com>
2022-12-15 17:36:23 -0600

    Updated to conform to pycodestyle

--- models/player.py
+++ models/player.py
@@ -11,14 +11,15 @@ YELLOW = (250, 228, 25)
 BLUE = (22, 114, 184)
 BLACK = (30, 30, 30)
 
+
 class Player(pygame.sprite.Sprite):
     """
     Defines the player sprite and its behavior.
 
     The player is represented by a red rectangle. The user can move the player
-    left and right using the left and right arrow keys, and shoot a bullet using
-    the space bar. The player sprite is bound by the left and right edges of the
-    screen, and bullets can only be shot once every 600ms.
+    left and right using the left and right arrow keys, and shoot a bullet
+    using the space bar. The player sprite is bound by the left and right
+    edges of the screen, and bullets can only be shot once every 600ms.
     """
     # Class attributes
     player_width = 60
@@ -31,9 +32,11 @@ class Player(pygame.sprite.Sprite):
 
         Args:
             pos: A tuple specifying the (x, y) position of the player sprite.
-            speed: An integer specifying the speed at which the player sprite moves.
+            speed: An integer specifying the speed at which the player
+            sprite moves.
         """
-        # Call the `__init__()` method of the superclass (`pygame.sprite.Sprite`)
+        # Call the `__init__()` method of the superclass
+        # (`pygame.sprite.Sprite`)
         super().__init__()
 
         # Create a new surface representing the player sprite
@@ -41,7 +44,7 @@ class Player(pygame.sprite.Sprite):
 
         # Set the position of the player sprite to the given position
         self.image.fill(self.player_color)
-        self.rect = self.image.get_rect(midbottom = pos)
+        self.rect = self.image.get_rect(midbottom=pos)
 
         # Set the speed of the player sprite to the given speed
         self.speed = speed
@@ -53,7 +56,6 @@ class Player(pygame.sprite.Sprite):
 
         # Bring in the laser
         self.bullets = pygame.sprite.Group()
-
 
     def update_player_color(self):
         """Updates player color"""
@@ -71,7 +73,6 @@ class Player(pygame.sprite.Sprite):
         self.image.fill(next_color)
         self.player_color = next_color
 
-
     def get_input(self):
         """
         Gets input from the user and updat  es the state of the player sprite
@@ -80,17 +81,18 @@ class Player(pygame.sprite.Sprite):
         # Get the keys that are currently being pressed
         keys = pygame.key.get_pressed()
 
-        # If the right arrow key is being pressed, move the player sprite to the
-        # right
+        # If the right arrow key is being pressed, move the player sprite to
+        # the right
         if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
 
-        # If the left arrow key is being pressed, move the player sprite to the left
+        # If the left arrow key is being pressed, move the player sprite to
+        # the left
         elif keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
 
-        # If the space bar is being pressed and the player sprite is ready to fire a
-        # bullet, shoot a bullet and start the bullet recharge timer
+        # If the space bar is being pressed and the player sprite is ready to
+        # fire a bullet, shoot a bullet and start the bullet recharge timer
         if keys[pygame.K_SPACE] and self.ready:
             self.shoot_bullet()
             self.ready = False
@@ -98,8 +100,8 @@ class Player(pygame.sprite.Sprite):
 
     def recharge(self):
         """
-        Checks if a bullet has been fired, and recharges the player sprite's bullet
-        if the bullet cooldown time has been reached.
+        Checks if a bullet has been fired, and recharges the player sprite's
+        bullet if the bullet cooldown time has been reached.
         """
         # Check if a bullet has been fired
         if self.ready is not True:
@@ -113,32 +115,37 @@ class Player(pygame.sprite.Sprite):
 
     def constraint(self):
         """
-        Constrains the player sprite's position to the left and right edges of the
-        screen.
+        Constrains the player sprite's position to the left and right edges of
+        the screen.
         """
         # Import the `screen_width` variable from the `main` module
         from main import screen_width
 
-        # If the player sprite's left edge is less than or equal to 0, set the left
-        # edge to 0
+        # If the player sprite's left edge is less than or equal to 0, set the
+        # left edge to 0
         if self.rect.left <= 0:
             self.rect.left = 0
 
-        # If the player sprite's right edge is greater than or equal to the screen
-        # width, set the right edge to the screen width
+        # If the player sprite's right edge is greater than or equal to the
+        # screen width, set the right edge to the screen width
         if self.rect.right >= screen_width:
             self.rect.right = screen_width
 
     # def get_color(self):
     #     return self.color
 
-    def shoot_bullet(self):       
+    def shoot_bullet(self):
         """
         Shoots a bullet
         """
         text = "COLOR"
         bullet_speed = -8
-        self.bullets.add(Bullet(self.rect.center, bullet_speed, self.rect.bottom, self.player_color))
+        self.bullets.add(Bullet(self.rect.center,
+                                bullet_speed,
+                                self.rect.bottom,
+                                self.player_color
+                                )
+                         )
         # Pew
         self.update_player_color()
         print('Pew but with ', end="")

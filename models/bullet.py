75870e53cafbead115d8693376108a7eaf13689e
bsbanotto <ben.sbanotto@gmail.com>
2022-12-15 10:15:29 -0600

    Updated standard color definitions to match bubble png files

--- models/bullet.py
+++ models/bullet.py
@@ -3,18 +3,18 @@ import random
 
 # Color definitions to randomly change color of bullets
 # White=1, Red=2, Green=3, Yellow=4, Blue=5
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-YELLOW = (255, 255, 0)
-BLUE = (0, 0, 255)
+WHITE = (202, 213, 218)
+RED = (232, 53, 38)
+GREEN = (87, 171, 65)
+YELLOW = (250, 228, 25)
+BLUE = (22, 114, 184)
+BLACK = (30, 30, 30)
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, pos, speed, screen_height, color):
         super().__init__()
         self.image = pygame.Surface((16, 16))
         self.image.fill(color)
-        # pygame.draw.circle(self.image, (0, 0, 0), pos, 8)
         self.rect = self.image.get_rect(center = pos)
         self.speed = speed
         self.height_y_constraint = screen_height
